06cb517c1473ecf2bd4efb9bf18aa8dbf2cc685e
nannojaved <paijyj@gmail.com>
2024-12-11 22:45:23 +0100

    Add project files.

---
+++ ASPDotNet_MVC_Dropdownlist/Controllers/DropdownController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDotNet_MVC_Dropdownlist.Controllers
+{
+    public class DropdownController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+        public IActionResult InlineInView()
+        {
+            return View();
+        }
+
+        static void ShowSelectedItem()
+        {
+
+        }
+    }
+}
---
+++ ASPDotNet_MVC_Dropdownlist/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPDotNet_MVC_Dropdownlist.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
